f74177cbee4cc9539a4a94dde8ed16bb36191d6d
letusget <a14705129921@163.com>
2022-01-18 23:25:19 +0800

    commit on 1.19

--- lesson25/poll.c
+++ lesson25/poll.c
@@ -6,6 +6,8 @@
 #include <sys/time.h>
 #include <sys/types.h>
 #include <sys/select.h>
+#include <poll.h>
+
 
 int main()
 {
@@ -25,23 +27,30 @@ int main()
 
     //判断 socket 和 bing 的返回值是否正确
 
-    //创建一个 fd_set 的集合，
-    fd_set rdset,tmp;
-    FD_ZERO(&rdset);
-    FD_SET(lfd,&rdset);
-    int maxfd=lfd;  //要检测的文件描述符的最大值
+    //初始化检测的文件描述符数组
+    struct pollfd fds[1024];    //该大小可以根据需要修改, 可以定义为一个宏值，方便修改
+    //对每一个结构体中的成员进行初始化
+    for(int i=0;i<1024;i++)
+    {
+        fds[i].fd=-1;   //初始时 文件描述符集合为无效
+        fds[i].events = POLLIN;   //初始为检测读事件
+    }
+    //初始时的文件描述符集合
+    fds[0].fd=lfd;
+   
+   //最后一个有效数组元素的下标
+    int nfds=0;
 
     while (1)
     {
-        //为了防止内核将 rdset 中的数据全部修改，下次使用时就是 内核修改完的数据了，这里使用tmp 来保存该数据
-        //使内核修改tmp，而一直保留rdset 
-        tmp=rdset;
-        int ret = select(maxfd+1,&tmp,NULL,NULL,NULL);
+        //调用 poll 系统函数，让内核检测那些文件描述符有数据
+        int ret = poll(fds,nfds+1,-1);    //如果没有数据就阻塞
+        
 
         //如果打开失败，就直接退出
         if(ret==-1)
         {
-            perror("select");
+            perror("poll");
             exit(-1);
         }
         else if(ret==0)
@@ -54,7 +63,7 @@ int main()
             //返回发生变换的文件个数
 
             //下面判断
-            if(FD_ISSET(lfd,&tmp))
+            if(fds[0].revents & POLLIN)
             {
                 //如果有新的客户端连接进来
                 struct sockaddr_in cliaddr;
@@ -65,20 +74,30 @@ int main()
                 //printf("client's ip is %s, and port is %d\n", cliaddr.sin_addr,cliaddr.sin_port);
 
                 //将新的文件描述符 放入集合中
-                FD_SET(cfd,&rdset);
+                for (int i = 1; i < 1024; i++)  //因为0 是已经监听的文件描述符了
+                {
+                    //判断时候有连接
+                    if (fds[i].fd==-1)
+                    {
+                        fds[i].fd=cfd;
+                        fds[i].events = POLLIN;
+                        break;
+                    }
+                }
+                
                 //更新最大的文件描述符
-                maxfd=maxfd>cfd?maxfd:cfd;
+                nfds = nfds > cfd ? nfds : cfd ;
 
             }
 
             //遍历寻找，是否有其他的文件描述符变化
-            for (int i = lfd+1; i <= maxfd; i++)
+            for (int i = 1; i <= nfds; i++) //从第一个索引开始遍历
             {
-                if(FD_ISSET(i,&tmp))
+                if(fds[i].revents & POLLIN)
                 {
                     //说明这个文件描述符对应的客户端发送了数据
                     char buf[1024]={0};
-                    int len=read(i,buf,sizeof(buf));
+                    int len=read(fds[i].fd,buf,sizeof(buf));
 
                     if(len==-1)
                     {
@@ -94,13 +113,13 @@ int main()
                         close(i);
 
                         //移除该文件描述符
-                        FD_CLR(i,&rdset);
+                        fds[i].fd=-1;
                     }
                     else if(len>0)
                     {
                         printf("read buf = %s\n",buf);
                         //回写内容,将 客户端发送的数据，再次返回给客户端
-                        write(i,buf,strlen(buf)+1);
+                        write(fds[i].fd,buf,strlen(buf)+1);
                     }
                 }
                
